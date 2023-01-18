264564894c1c6aaed978b30f88809e56e49a8e78
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-01-18 23:32:10 +0300

    used generic repository design pattern and added the project. Added entity's repository class and interface

--- ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ ETicaretAPI/Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ETicaretAPI.Application.Repositories;
 using ETicaretAPI.Persistence.Contexts;
+using ETicaretAPI.Persistence.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -14,9 +16,16 @@ namespace ETicaretAPI.Persistence
     {
         public static void AddPersistenceServices(this IServiceCollection services)
         {
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
 
+            services.AddSingleton<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddSingleton<ICustomerWriteRepository, CustomerWriteRepository>();
 
-            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddSingleton<IOrderReadRepository, OrderReadRepository>();
+            services.AddSingleton<IOrderWriteRepository, OrderWriteRepository>();
+
+            services.AddSingleton<IProductReadRepository, ProductReadRepository>();
+            services.AddSingleton<IProductWriteRepository, ProductWriteRepository>();
         }
     }
 }
---
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using ETicaretAPI.Application.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        readonly private IProductWriteRepository _productWriteRepository;
+        readonly private IProductReadRepository _productReadRepository;
+        public ProductController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+
+        [HttpGet]
+        public async void Get()
+        {
+            _productWriteRepository.AddRangeAsync(new()
+            {
+                new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 123, CreatedDate = DateTime.UtcNow, Stock = 12 },
+                new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 223, CreatedDate = DateTime.UtcNow, Stock = 22 },
+                new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 323, CreatedDate = DateTime.UtcNow, Stock = 32 },
+            });
+            await _productWriteRepository.SaveAsync();
+        }
+    }
+}
