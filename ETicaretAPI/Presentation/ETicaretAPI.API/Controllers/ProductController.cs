b510463b591cdaed37132eeb294ae4d53c463aa3
Kerem-Kaplan <keremkaplan1000@gmail.com>
2023-01-19 21:19:58 +0300

    added entity framework tracking optimization

--- ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductController.cs
+++ ETicaretAPI/Presentation/ETicaretAPI.API/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,15 +18,30 @@ namespace ETicaretAPI.API.Controllers
         }
 
         [HttpGet]
-        public async void Get()
+        public async Task Get()
         {
-            _productWriteRepository.AddRangeAsync(new()
-            {
-                new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 123, CreatedDate = DateTime.UtcNow, Stock = 12 },
-                new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 223, CreatedDate = DateTime.UtcNow, Stock = 22 },
-                new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 323, CreatedDate = DateTime.UtcNow, Stock = 32 },
-            });
+            //await _productWriteRepository.AddRangeAsync(new()
+            //{
+            //    new() { Id = Guid.NewGuid(), Name = "Product 1", Price = 123, CreatedDate = DateTime.UtcNow, Stock = 12 },
+            //    new() { Id = Guid.NewGuid(), Name = "Product 2", Price = 223, CreatedDate = DateTime.UtcNow, Stock = 22 },
+            //    new() { Id = Guid.NewGuid(), Name = "Product 3", Price = 323, CreatedDate = DateTime.UtcNow, Stock = 32 },
+            //});
+            //await _productWriteRepository.SaveAsync();
+
+            Product p = await _productReadRepository.GetByIdAsync("4d069ad8-05cc-4973-a2db-eb0b348654d6", false);
+            p.Name = "Mehmet";
             await _productWriteRepository.SaveAsync();
+
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
+            return Ok(product);
+
+
+
         }
     }
 }
